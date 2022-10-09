476cb727a5f784a4ba5b1dc0a934329720051c6f
anushreeann <anushreeann99@gmail.com>
2022-10-09 23:26:40 +0530

    first commit

---
+++ string.py
@@ -0,0 +1,15 @@
+value ="welcome to python"
+print(value[2])
+print(len(value))
+print(value +' world')
+print(value.upper())
+d="anushree is a mca student"
+print(d.strip())
+print(d.find('a'))
+print(d.replace('mca','MCA'))
+print(d[:8])
+print(d[0:1000])
+x,y,z="orange","banana","cherry"
+print(x)
+print(y)
+
